59a1ff3b75432ec3dcfe3a091fc871df8538b26e
shikharaa <shikhara1997@gmail.com>
2023-06-09 11:13:49 -0700

    App changes for configMap

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var hostname = os.hostname();
 var app = express()
 
 app.get('/', function(req, res) {
-  res.send('Hello world from ' + hostname + '! Your app is up and running!\n')
+  //res.send('Hello world from ' + hostname + '! Your app is up and running!\n') //directly running
+  res.send(process.env.MESSAGE + '\n') // for getting value form config 
 })
 app.listen(8080, function() {
   console.log('Sample app is listening on port 8080.')
